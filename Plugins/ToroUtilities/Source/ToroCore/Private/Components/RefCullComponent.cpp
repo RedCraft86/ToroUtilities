5b4aebd692596f291edf0150d4126b92f0f90ab5
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2025-11-05 14:47:04 -0500

    Reference culling component

---
+++ Plugins/ToroUtilities/Source/ToroCore/Private/Components/RefCullComponent.cpp
@@ -0,0 +1,61 @@
+﻿// Copyright (C) RedCraft86. Licensed under LGPL-3.0 (See LICENSE file for details).
+
+#include "Components/RefCullComponent.h"
+
+URefCullComponent::URefCullComponent(): bNullChecks(false), bAffectTicking(false)
+{
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	PrimaryComponentTick.TickGroup = TG_DuringPhysics;
+	PrimaryComponentTick.TickInterval = 0.5f;
+}
+
+void URefCullComponent::AddRequest(const UObject* InRequester)
+{
+	if (InRequester && !Requests.Contains(InRequester))
+	{
+		Requests.Add(InRequester);
+		UpdateCullingState();
+	}
+}
+
+void URefCullComponent::RemoveRequest(const UObject* InRequester)
+{
+	if (InRequester && Requests.Remove(InRequester) > 0)
+	{
+		UpdateCullingState();
+	}
+}
+
+void URefCullComponent::UpdateCullingState()
+{
+	for (auto It = Requests.CreateIterator(); It; ++It)
+	{
+		if (!It->IsValid()) It.RemoveCurrent();
+	}
+
+	AActor* Owner = GetOwner();
+	Owner->SetActorHiddenInGame(Requests.IsEmpty());
+	if (bAffectTicking && Requests.IsEmpty())
+	{
+		OriginalTickState = Owner->IsActorTickEnabled();
+		Owner->SetActorTickEnabled(false);
+	}
+	else if (OriginalTickState.IsSet())
+	{
+		Owner->SetActorTickEnabled(OriginalTickState.GetValue());
+		OriginalTickState.Reset();
+		
+	}
+}
+
+void URefCullComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	SetComponentTickEnabled(bNullChecks);
+}
+
+void URefCullComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* TickFunc)
+{
+	Super::TickComponent(DeltaTime, TickType, TickFunc);
+	UpdateCullingState();
+}
---
+++ Plugins/ToroUtilities/Source/ToroCore/Public/Components/RefCullComponent.h
@@ -0,0 +1,41 @@
+﻿// Copyright (C) RedCraft86. Licensed under LGPL-3.0 (See LICENSE file for details).
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "RefCullComponent.generated.h"
+
+/**
+ * A component that handles hiding (and/or ticking) of its parent actor depending on if there are any requests for it.
+ */
+UCLASS(NotBlueprintable, DisplayName = "Reference Culling", ClassGroup = (Optimization), meta = (BlueprintSpawnableComponent))
+class TOROCORE_API URefCullComponent final : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	URefCullComponent();
+
+	/* Do periodic checks to ensure all requests are non-null. Otherwise, only check when adding or removing one. */
+	UPROPERTY(EditAnywhere, Category = Settings)
+		bool bNullChecks;
+
+	UPROPERTY(EditAnywhere, Category = Settings)
+		bool bAffectTicking;
+
+	UFUNCTION(BlueprintCallable, Category = ReferenceCulling)
+		void AddRequest(const UObject* InRequester);
+
+	UFUNCTION(BlueprintCallable, Category = ReferenceCulling)
+		void RemoveRequest(const UObject* InRequester);
+
+protected:
+
+	TOptional<bool> OriginalTickState;
+	TSet<TWeakObjectPtr<const UObject>> Requests;
+
+	void UpdateCullingState();
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* TickFunc) override;
+};
