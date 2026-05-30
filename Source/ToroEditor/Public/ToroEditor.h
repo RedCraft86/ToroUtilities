fc1be2fe9291ddc550c2d8a62e4c846cf6934b3c
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-05-30 13:34:50 -0400

    Plugin dir getter for editor resources

--- Source/ToroEditor/Public/ToroEditor.h
+++ Source/ToroEditor/Public/ToroEditor.h
@@ -3,12 +3,22 @@
 #pragma once
 
 #include "Modules/ModuleManager.h"
+#include "Interfaces/IPluginManager.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogToroEditor, All, All)
 
 class TOROEDITOR_API FToroEditorModule final : public IModuleInterface
 {
 public:
+
+    static FString GetPluginDirectory()
+    {
+        static FString Path = IPluginManager::Get().FindPlugin(TEXT("ToroUtilities"))->GetBaseDir();
+        return Path;
+    }
+
+private:
+
     virtual void StartupModule() override;
     virtual void ShutdownModule() override;
 };
