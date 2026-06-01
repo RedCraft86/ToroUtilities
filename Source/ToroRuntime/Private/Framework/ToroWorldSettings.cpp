02db102f5bd9323bd1e050a8c41758746d75c2f6
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-06-01 16:18:13 -0400

    World settings w/ master post process

--- Source/ToroRuntime/Private/Framework/ToroWorldSettings.cpp
+++ Source/ToroRuntime/Private/Framework/ToroWorldSettings.cpp
@@ -1,13 +1,171 @@
 // Copyright (C) RedCraft86 2026. Licensed under LGPL-3.0 (See LICENSE file for details).
 
 #include "Framework/ToroWorldSettings.h"
+#include "Libraries/ToroConsoleLibrary.h"
+#include "Materials/MaterialInstanceDynamic.h"
 
 AToroWorldSettings::AToroWorldSettings()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bTickEvenWhenPaused = true;
 	PrimaryActorTick.bStartWithTickEnabled = true;
-	PrimaryActorTick.TickGroup = TG_PrePhysics;
+	PrimaryActorTick.TickGroup = TG_DuringPhysics;
 
 	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
 	SetRootComponent(SceneRoot);
+
+	PostProcess = CreateDefaultSubobject<UPostProcessComponent>(TEXT("PostProcess"));
+	PostProcess->SetupAttachment(SceneRoot);
+	PostProcess->BlendWeight = 1.0f;
+	PostProcess->Priority = -1.0f;
+	PostProcess->bUnbound = true;
+	PostProcess->bEnabled = true;
+
+	PostProcessTickInterval = 0.1f;
+	PostProcessTick = 0.0f;
+
+	PostProcessing.bOverride_AutoExposureMethod = true;
+	PostProcessing.AutoExposureMethod = AEM_Manual;
+
+	PostProcessing.bOverride_AutoExposureBias = true;
+	PostProcessing.AutoExposureBias = 11.0f;
+
+	PostProcessing.bOverride_BloomMethod = true;
+	PostProcessing.BloomMethod = EBloomMethod::BM_FFT;
+
+	PostProcess->Settings = PostProcessing;
+
+	bEnableAutoLODGeneration = false; // Include Actor in HLOD option
+
+	SetCanBeDamaged(false);
 }
+
+bool AToroWorldSettings::IsUsingLumenGI()
+{
+	// Ideally, other post-processes should override GI to only DISABLE Lumen, never ENABLE it. This one keeps it on.
+	// If GlobalIlluminationQuality == 0 (Low), GI is off even when set to Lumen.
+	return UserSettings->GetGlobalIlluminationQuality() > 0 && 
+		(!PostProcess->Settings.bOverride_DynamicGlobalIlluminationMethod // Assume project default is being ON
+		|| PostProcess->Settings.DynamicGlobalIlluminationMethod == EDynamicGlobalIlluminationMethod::Lumen);
+}
+
+const FPostProcessSettings& AToroWorldSettings::GetPostProcessSettings() const
+{
+	return PostProcess->Settings;
+}
+
+UMaterialInterface* AToroWorldSettings::FindBlendable(const UMaterialInterface* InMaterial) const
+{
+	if (InMaterial)
+	{
+		for (const FWeightedBlendable& Blendable : PostProcessing.WeightedBlendables.Array)
+		{
+			if (UMaterialInterface* Material = Cast<UMaterialInterface>(Blendable.Object))
+			{
+				if (Material == InMaterial)
+				{
+					return Material;
+				}
+
+				const UMaterialInstanceDynamic* MID = Cast<UMaterialInstanceDynamic>(Material);
+				if (MID && MID->Parent == InMaterial)
+				{
+					return Material;
+				}
+			}
+		}
+	}
+
+	return nullptr;
+}
+
+UMaterialInterface* AToroWorldSettings::FindOrAddBlendable(UMaterialInterface* InMaterial, const bool bDynamic)
+{
+	if (InMaterial)
+	{
+		UMaterialInterface* Blendable = FindBlendable(InMaterial);
+		if (Blendable)
+		{
+			// Set weight to 1 just in case
+			PostProcessing.AddBlendable(Blendable, 1.0f);
+			return Blendable;
+		}
+
+		Blendable = bDynamic ? UMaterialInstanceDynamic::Create(InMaterial, this) : InMaterial;
+		PostProcessing.AddBlendable(Blendable, 1.0f);
+		PostProcessTick = PostProcessTickInterval;
+		UpdatePostProcess();
+
+		return Blendable;
+	}
+
+	return nullptr;
+}
+
+void AToroWorldSettings::RemoveBlendable(UMaterialInterface* InMaterial)
+{
+	if (UMaterialInterface* Blendable = FindBlendable(InMaterial))
+	{
+		PostProcessing.RemoveBlendable(Blendable);
+		PostProcessTick = PostProcessTickInterval;
+		UpdatePostProcess();
+	}
+}
+
+void AToroWorldSettings::UpdatePostProcess()
+{
+#if WITH_EDITOR
+	if (FApp::IsGame())
+#endif
+	{
+		// TODO ELumenUsageMode LumenMode = UserSettings->GetLumenMode();
+		//
+		// Settings.bOverride_ReflectionMethod = true;
+		// Settings.ReflectionMethod = LumenReflectionAllowed(LumenMode) 
+		// 	? EReflectionMethod::Lumen : EReflectionMethod::ScreenSpace;
+		//
+		// Settings.bOverride_DynamicGlobalIlluminationMethod = true;
+		// Settings.DynamicGlobalIlluminationMethod = LumenLightingAllowed(LumenMode) 
+		// 	? EDynamicGlobalIlluminationMethod::Lumen : EDynamicGlobalIlluminationMethod::ScreenSpace;
+
+		if (UserSettings->GetGlobalIlluminationQuality() >= 4)
+		{
+			PostProcessing.LumenRayLightingMode = ELumenRayLightingModeOverride::HitLighting;
+		}
+		else
+		{
+			PostProcessing.LumenRayLightingMode = UserSettings->GetReflectionQuality() >= 3
+				? ELumenRayLightingModeOverride::HitLightingForReflections
+				: ELumenRayLightingModeOverride::SurfaceCache;
+		}
+	}
+
+	PostProcess->Settings = PostProcessing;
+}
+
+void AToroWorldSettings::BeginPlay()
+{
+	Super::BeginPlay();
+	PostProcessTick = -1.0f; // For initial run
+
+	// Fixes motion blur issues when paused
+	GetWorld()->bIsCameraMoveableWhenPaused = true;
+}
+
+void AToroWorldSettings::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	if ((PostProcessTick -= DeltaSeconds) < 0.0f)
+	{
+		PostProcessTick = PostProcessTickInterval;
+		UpdatePostProcess();
+	}
+}
+
+#if WITH_EDITOR
+void AToroWorldSettings::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	UpdatePostProcess();
+}
+#endif
--- Source/ToroRuntime/Public/Framework/ToroWorldSettings.h
+++ Source/ToroRuntime/Public/Framework/ToroWorldSettings.h
@@ -3,11 +3,16 @@
 #pragma once
 
 #include "Helpers/WorldGetter.h"
+#include "DataTypes/CachedGetter.h"
 #include "GameFramework/WorldSettings.h"
+#include "Components/PostProcessComponent.h"
+#include "UserSettings/ToroGameUserSettings.h"
 #include "ToroWorldSettings.generated.h"
 
 /**
- * Specialized level-specific configuration.
+ * Custom implementation of AWorldSettings that serves as the centralized visual 
+ * and configuration baseline for the level. Also hosts a global UPostProcessComponent 
+ * with a dynamic Post-Process Material (Blendable) management.
  */
 UCLASS(NotPlaceable, Blueprintable, BlueprintType)
 class TORORUNTIME_API AToroWorldSettings : public AWorldSettings
@@ -25,8 +30,71 @@ public:
 		return IsValid(World) ? Cast<T>(World->GetWorldSettings()) : nullptr;
 	}
 
+	/** 
+	 * Checks if Lumen GI is currently enabled. Returns false if GI Quality is Low. 
+	 * @note Assumes it is on by default in the project and does NOT account for local post process volumes.
+	 */
+	UFUNCTION(BlueprintPure, Category = PostProcess)
+		bool IsUsingLumenGI();
+
+	/** 
+	 * Accesses the underlying PostProcessSettings structure.
+	 * @return Reference to the final global post-processing configuration.
+	 */
+	UFUNCTION(BlueprintPure, Category = PostProcess)
+		const FPostProcessSettings& GetPostProcessSettings() const;
+
+	/** 
+	 * Searches the current active blendable stack for a specific material.
+	 * @param InMaterial The material interface to search for.
+	 * @return The material if found in the stack (static or dynamic), otherwise nullptr.
+	 */
+	UFUNCTION(BlueprintPure, Category = PostProcess)
+		UMaterialInterface* FindBlendable(const UMaterialInterface* InMaterial) const;
+
+	/** 
+	 * Ensures a material is present in the global post-process stack.
+	 * @param InMaterial The base material to add.
+	 * @param bDynamic If true, creates a Material Instance Dynamic (MID) for runtime parameter modification.
+	 * @return The resulting material interface (Asset or MID).
+	 */
+	UFUNCTION(BlueprintCallable, Category = PostProcess)
+		UMaterialInterface* FindOrAddBlendable(UMaterialInterface* InMaterial, const bool bDynamic);
+
+	/** 
+	 * Removes a material from the post-process stack and tracking map.
+	 * @param InMaterial The material to remove.
+	 */
+	UFUNCTION(BlueprintCallable, Category = PostProcess)
+		void RemoveBlendable(UMaterialInterface* InMaterial);
+
 protected:
 
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Subobjects)
+	UPROPERTY(BlueprintReadOnly, Category = Subobjects)
 		TObjectPtr<USceneComponent> SceneRoot;
+
+	UPROPERTY(BlueprintReadOnly, Category = Subobjects)
+		TObjectPtr<UPostProcessComponent> PostProcess;
+
+	/** Time (in seconds) between user-setting synchronization. 0.0 for every frame (Runtime Only). */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = PostProcessing, meta = (ClampMin = 0.0f, UIMin = 0.0f))
+		float PostProcessTickInterval;
+
+	/** The baseline post-processing configuration for this specific world. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = PostProcessing, meta = (ShowOnlyInnerProperties))
+		FPostProcessSettings PostProcessing;
+
+	float PostProcessTick;
+	TCachedGetter<UToroGameUserSettings> UserSettings {[]
+	{
+		return UToroGameUserSettings::Get();
+	}};
+
+	void UpdatePostProcess();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+#if WITH_EDITOR
+	virtual void OnConstruction(const FTransform& Transform) override;
+#endif
 };
