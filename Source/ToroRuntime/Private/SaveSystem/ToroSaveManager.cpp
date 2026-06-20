0a3102e664f6c76328a4ebafddca8c2cb976b39f
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-06-20 15:27:22 -0400

    Impl save manager

---
+++ Source/ToroRuntime/Private/SaveSystem/ToroSaveManager.cpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2026 Tayzar Linn. Licensed under the GNU Lesser General Public License v3.0 (LGPL-3.0). 
+// See the LICENSE file in the project root, or <https://www.gnu.org/licenses/lgpl-3.0.html>.
+
+#include "SaveSystem/ToroSaveManager.h"
+
+UToroSaveGame* UToroSaveManager::GetSaveObject(const TSubclassOf<UToroSaveGame> Class)
+{
+	if (!Class)
+	{
+		return nullptr;
+	}
+
+	const TObjectPtr<UToroSaveGame>* Element = SaveObjects.Find(Class);
+	if (Element && *Element)
+	{
+		return *Element;
+	}
+
+	UToroSaveGame* NewSaveObj = NewObject<UToroSaveGame>(this, Class);
+	SaveObjects.Add(Class, NewSaveObj);
+	return NewSaveObj;
+}
---
+++ Source/ToroRuntime/Public/SaveSystem/ToroSaveManager.h
@@ -0,0 +1,56 @@
+// Copyright (C) 2026 Tayzar Linn. Licensed under the GNU Lesser General Public License v3.0 (LGPL-3.0). 
+// See the LICENSE file in the project root, or <https://www.gnu.org/licenses/lgpl-3.0.html>.
+
+#pragma once
+
+#include "ToroSaveGame.h"
+#include "Helpers/WorldGetter.h"
+#include "Engine/GameInstance.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ToroSaveManager.generated.h"
+
+/**
+ * A subsystem used to manage the lifecycle of save game objects for the entire duration of the game instance.
+ * Serves as a cache for save objects, ensuring that only one instance of a specific class exists at a time.
+ */
+UCLASS(NotBlueprintable, BlueprintType)
+class TORORUNTIME_API UToroSaveManager final : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	UToroSaveManager() {}
+
+	[[nodiscard]] static UToroSaveManager* Get(const UObject* ContextObject)
+	{
+		const UWorld* World = FWorldGetter::Get(ContextObject);
+		const UGameInstance* GI = IsValid(World) ? World->GetGameInstance() : nullptr;
+		return IsValid(GI) ? GI->GetSubsystem<UToroSaveManager>() : nullptr;
+	}
+
+	/**
+	 * Retrieves an existing save object from the cache or creates a new instance if none exists.
+	 * @param Class The specific UToroSaveGame class to find or instantiate.
+	 * @return The cached or newly created save object instance or null if Class is null.
+	 */
+	UFUNCTION(BlueprintCallable, Category = SaveSystem, meta = (DetermineOutputType = Class))
+		UToroSaveGame* GetSaveObject(UPARAM(meta = (AllowAbstract = false)) const TSubclassOf<UToroSaveGame> Class);
+
+	/**
+	 * Get or create a save object instance if no instance exist.
+	 * @tparam T The class type, which must derive from UToroSaveGame.
+	 * @return The instance of T, or nullptr if the system fails to create/find the object.
+	 */
+	template<typename T>
+	T* GetSaveObject()
+	{
+		static_assert(TIsDerivedFrom<T, UToroSaveGame>::Value, TEXT("T must derive from UToroSaveGame"));
+		return Cast<T>(GetSaveObject(T::StaticClass()));
+	}
+
+private:
+
+	UPROPERTY(Transient)
+		TMap<TSubclassOf<UToroSaveGame>, TObjectPtr<UToroSaveGame>> SaveObjects;
+};
