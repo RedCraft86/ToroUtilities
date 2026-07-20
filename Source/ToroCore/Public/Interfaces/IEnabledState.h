581958a6fb0160832c11493a9bf1523b34ef0b4b
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-07-19 22:39:45 -0400

    Other main interfaces

---
+++ Source/ToroCore/Public/Interfaces/IEnabledState.h
@@ -0,0 +1,45 @@
+// Copyright (C) 2026 Tayzar Linn. Licensed under LGPL-3.0, see project LICENSE file.
+
+#pragma once
+
+#include "UObject/Interface.h"
+#include "Components/ActorComponent.h"
+#include "IEnabledState.generated.h"
+
+/**
+ * General use interface to add an arbiratary Enabled State functions to objects. (UObject Wrapper)
+ */
+UINTERFACE()
+class UEnabledState : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * General use interface to add an arbiratary Enabled State functions to objects.
+ */
+class TOROCORE_API IEnabledState
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = ToroCore)
+		void SetEnabledState(const bool bEnabled);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = ToroCore)
+		bool GetEnabledState() const;
+
+	static void SetEnabled(UObject* Target, const bool bEnabled)
+	{
+		if (Target && !Target->Implements<UEnabledState>())
+		{
+			Execute_SetEnabledState(Target, bEnabled);
+		}
+	}
+
+	static bool IsEnabled(const UObject* Target)
+	{
+		return Target && (!Target->Implements<UEnabledState>() || Execute_GetEnabledState(Target));
+	}
+};
---
+++ Source/ToroCore/Public/Interfaces/IExitable.h
@@ -0,0 +1,33 @@
+// Copyright (C) 2026 Tayzar Linn. Licensed under LGPL-3.0, see project LICENSE file.
+
+#pragma once
+
+#include "UObject/Interface.h"
+#include "IExitable.generated.h"
+
+/**
+ * General use interface to add an arbiratary "Exit" function to objects. (UObject Wrapper)
+ */
+UINTERFACE()
+class UExitable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * General use interface to add an arbiratary "Exit" function to objects.
+ */
+class TOROCORE_API IExitable
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = ToroCore)
+		bool Exit(const UObject* Instigator);
+
+	static bool RequestExit(UObject* Target, const UObject* Instigator)
+	{
+		return Target && Target->Implements<UExitable>() && Execute_Exit(Target, Instigator);
+	}
+};
