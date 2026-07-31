10e97cb32f3765b803a083d27d4f30d40ce788c9
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-07-31 19:54:07 -0400

    Impl instanced mesh actors

---
+++ Source/ToroCore/Private/Actors/ISMActors.cpp
@@ -0,0 +1,78 @@
+// Copyright (C) 2026 Tayzar Linn. Licensed under GNU Lesser General Public License v3.0, see project LICENSE file.
+
+#include "Actors/ISMActors.h"
+#include "Components/InstancedStaticMeshComponent.h"
+#include "Components/HierarchicalInstancedStaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+
+AInstancedStaticMeshActor::AInstancedStaticMeshActor(const FObjectInitializer& ObjectInit)
+	: Super(ObjectInit.SetDefaultSubobjectClass<UInstancedStaticMeshComponent>(StaticMeshComponentName))
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	bCanBeInCluster = GetClass() == AInstancedStaticMeshActor::StaticClass();
+
+	if (!IsRunningCommandlet() && !IsRunningDedicatedServer())
+	{
+		if (UStaticMeshComponent* MeshComp = GetStaticMeshComponent())
+		{
+			static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshFinder(
+				TEXT("/Engine/BasicShapes/Cube.Cube"));
+			if (MeshFinder.Succeeded()) MeshComp->SetStaticMesh(MeshFinder.Object);
+
+			static ConstructorHelpers::FObjectFinder<UMaterialInterface> MaterialFinder(
+				TEXT("/Engine/BasicShapes/BasicShapeMaterial.BasicShapeMaterial"));
+			if (MaterialFinder.Succeeded()) MeshComp->SetMaterial(0, MaterialFinder.Object);
+		}
+	}
+}
+
+#if WITH_EDITOR
+void AInstancedStaticMeshActor::UpdateInstances() const
+{
+	UInstancedStaticMeshComponent* ISMC = GetMeshComponent();
+	if (ISMC && !bLockInstances)
+	{
+		ISMC->ClearInstances();
+		ISMC->AddInstances(Instances, false);
+	}
+}
+
+void AInstancedStaticMeshActor::CopyInstancesFromComponent()
+{
+	bLockInstances = true;
+	if (const UInstancedStaticMeshComponent* ISMC = GetMeshComponent())
+	{
+		const int32 NumInstances = ISMC->GetNumInstances();
+		Instances.Empty(NumInstances);
+
+		for (int32 i = 0; i < NumInstances; i++)
+		{
+			FTransform T;
+			ISMC->GetInstanceTransform(i, T, false);
+			Instances.Add(T);
+		}
+	}
+	bLockInstances = false;
+}
+
+void AInstancedStaticMeshActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	if (Instances.IsEmpty())
+	{
+		Instances.Add(FTransform::Identity);
+	}
+	UpdateInstances();
+}
+#endif
+
+AHierarchicalInstancedStaticMeshActor::AHierarchicalInstancedStaticMeshActor(const FObjectInitializer& ObjectInit)
+	: Super(ObjectInit.SetDefaultSubobjectClass<UHierarchicalInstancedStaticMeshComponent>(StaticMeshComponentName))
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	bCanBeInCluster = GetClass() == AHierarchicalInstancedStaticMeshActor::StaticClass();
+}
---
+++ Source/ToroCore/Public/Actors/ISMActors.h
@@ -0,0 +1,57 @@
+// Copyright (C) 2026 Tayzar Linn. Licensed under GNU Lesser General Public License v3.0, see project LICENSE file.
+
+#pragma once
+
+#include "Engine/StaticMeshActor.h"
+#include "ISMActors.generated.h"
+
+/**
+ * An optimized Actor for rendering large numbers of the same Static Mesh asset.
+ * This class replaces the standard Static Mesh Component with an Instanced version (ISM).
+ */
+UCLASS(meta = (ChildCanTick = true))
+class TOROCORE_API AInstancedStaticMeshActor : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AInstancedStaticMeshActor(const FObjectInitializer& ObjectInit);
+
+	template<typename T = UInstancedStaticMeshComponent>
+	T* GetMeshComponent() const { return Cast<T>(GetStaticMeshComponent()); }
+
+#if WITH_EDITORONLY_DATA
+	/** 
+	* The master list of transforms for all instances.
+	*/
+	UPROPERTY(Category = StaticMesh, EditAnywhere, meta = (MakeEditWidget, DisplayPriority = 1))
+		TArray<FTransform> Instances = { FTransform::Identity };
+#endif
+
+#if WITH_EDITOR
+	void UpdateInstances() const;
+	void CopyInstancesFromComponent();
+
+protected:
+
+	bool bLockInstances = false;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+#endif
+};
+
+/**
+ * An extension of AInstancedStaticMeshActor that utilizes HISMs.
+ * Recommended for meshes that require LOD (Level of Detail) support and 
+ * occlusion culling for high-density environments.
+ */
+UCLASS(meta = (ChildCanTick = true))
+class TOROCORE_API AHierarchicalInstancedStaticMeshActor : public AInstancedStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AHierarchicalInstancedStaticMeshActor(const FObjectInitializer& ObjectInit);
+};
