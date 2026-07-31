097499c12144350430a536c6635377e0da9d0dec
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-07-30 22:25:00 -0400

    static utility classes

---
+++ Source/ToroCore/Public/Helpers/LatentInfo.h
@@ -0,0 +1,51 @@
+// Copyright (C) 2026 Tayzar Linn. Licensed under LGPL-3.0, see project LICENSE file.
+
+#pragma once
+
+#include "Engine/LatentActionManager.h"
+
+/**
+ * A static utility class for creating FLatentActionInfo structs.
+ * 
+ * <b>Usage Example:</b>
+ * <pre>
+ *     FLatentActionInfo Info = FLatentMaker::Make(GET_MEMBER_NAME_CHECKED(UMyClass, AfterDelay), this);
+ *     UKismetSystemLibrary::Delay(GetWorld(), 2.0f, Info);
+ * </pre>
+ */
+class FLatentInfo
+{
+	static inline uint16 LatentId = 1000; 
+
+public:
+	
+	/**
+	 * Creates a fully initialized FLatentActionInfo struct.
+	 * @param FuncName The name of the function to execute once the latent action completes.
+	 * @param Target   The object on which to call the completion function.
+	 * @return A unique FLatentActionInfo configured for the current execution context.
+	 * @note This method is not thread-safe. Latent actions should generally be initialized on the Game Thread.
+	 */
+	[[nodiscard]] static FLatentActionInfo Make(const TCHAR* FuncName = TEXT(""), UObject* Target = nullptr)
+	{
+		checkf(IsInGameThread(), TEXT("FLatentInfo::Make(...) should only be called on the Game Thread."))
+		return FLatentActionInfo(0, NextId(), FuncName, Target);
+	}
+
+	/**
+	 * Increments and returns the next available LinkID. 
+	 * Automatically wraps back to 1000 if the <c>UINT16_MAX</c> limit 
+	 * is reached to maintain a consistent range of safe IDs that avoid potential collisions with BP code.
+	 * @return A unique unsigned 16-bit integer ID.
+	 * @note This method is not thread-safe. Latent actions should generally be initialized on the Game Thread.
+	 */
+	[[nodiscard]] static uint16 NextId()
+	{
+		checkf(IsInGameThread(), TEXT("FLatentInfo::NextId() should only be called on the Game Thread."))
+		if (LatentId == UINT16_MAX)
+		{
+			LatentId = 1000;
+		}
+		return LatentId++;
+	}
+};
---
+++ Source/ToroCore/Public/Helpers/WorldGetter.h
@@ -0,0 +1,71 @@
+// Copyright (C) 2026 Tayzar Linn. Licensed under LGPL-3.0, see project LICENSE file.
+
+#pragma once
+
+#include "Engine/Engine.h"
+
+/**
+ * A static utility class for UWorld fetching and caching.
+ * 
+ * <b>Usage Example:</b>
+ * <pre>
+ *     UGameplayStatics::GetPlayerController(FWorldGetter::Get(OptionalContext), 0);
+ * </pre>
+ */
+class TOROCORE_API FWorldGetter final
+{
+	static inline TWeakObjectPtr<UWorld> CachedWorld = nullptr;
+
+public:
+
+	/**
+	 * If a valid World is already cached, it is returned immediately. Otherwise, it 
+	 * attempts to resolve the World via the provided Context object or GEngine globals.
+	 * @param Context (Optional) An object belonging to the desired world. Recommended to 
+	 *        always try and provide one as it is required for the first call per world.
+	 * @return A pointer to the current UWorld, or nullptr if no world could be resolved.
+	 */
+	[[nodiscard]] static UWorld* Get(const UObject* Context = nullptr)
+	{
+		if (CachedWorld.IsValid())
+		{
+			return CachedWorld.Get();
+		}
+
+		if (GEngine)
+		{
+			CachedWorld = GEngine->GetWorldFromContextObject(Context, EGetWorldErrorMode::ReturnNull);
+			if (!CachedWorld.IsValid())
+			{
+				CachedWorld = GEngine->GetCurrentPlayWorld();
+			}
+		}
+		else
+		{
+			CachedWorld = GWorld.GetReference();
+		}
+
+		return CachedWorld.Get();
+	}
+
+	/**
+	 * Manually updates the internal cache with a specific World pointer.
+	 * @param InWorld The world instance to cache.
+	 */
+	static void SetWorld(UWorld* InWorld)
+	{
+		if (InWorld)
+		{
+			CachedWorld = InWorld;
+		}
+	}
+
+	/**
+	 * Call to clear the weak pointer so subsequent Get() calls re-evaluate the context.
+	 * @note Automatically called during map changes in <c>ToroCore.cpp</c>
+	 */
+	static void Reset()
+	{
+		CachedWorld.Reset();
+	}
+};
