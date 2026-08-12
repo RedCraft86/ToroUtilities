1103065e040270a13e63946696cd1b4bcc4b7534
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-08-11 22:14:33 -0400

    Use TNumericLimits instead of raw macro

--- Source/ToroCore/Public/Helpers/LatentInfo.h
+++ Source/ToroCore/Public/Helpers/LatentInfo.h
@@ -22,7 +22,7 @@ public:
 	/**
 	 * Creates a fully initialized FLatentActionInfo struct.
 	 * @param FuncName The name of the function to execute once the latent action completes.
-	 * @param Target   The object on which to call the completion function.
+	 * @param Target The object on which to call the completion function.
 	 * @return A unique FLatentActionInfo configured for the current execution context.
 	 * @note This method is not thread-safe. Latent actions should generally be initialized on the Game Thread.
 	 */
@@ -34,7 +34,7 @@ public:
 
 	/**
 	 * Increments and returns the next available LinkID.
-	 * Automatically wraps back to 1000 if the <c>UINT16_MAX</c> limit
+	 * Automatically wraps back to 1000 if the <c>UINT16_MAX</c> limit.
 	 * is reached to maintain a consistent range of safe IDs that avoid potential collisions with BP code.
 	 * @return A unique unsigned 16-bit integer ID.
 	 * @note This method is not thread-safe. Latent actions should generally be initialized on the Game Thread.
@@ -42,7 +42,7 @@ public:
 	[[nodiscard]] static uint16 NextId()
 	{
 		checkf(IsInGameThread(), TEXT("FLatentInfo::NextId() should only be called on the Game Thread."))
-		if (LatentId == UINT16_MAX)
+		if (LatentId == TNumericLimits<uint16>::Max())
 		{
 			LatentId = 1000;
 		}
